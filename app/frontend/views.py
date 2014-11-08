5c4739e189e38faa8767cb2c3dc23b92e54ed65b
ondoheer <ondoheer@gmail.com>
2014-11-08 16:01:57 -0500

    actualice git ignore

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,9 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-frontend = Blueprint('frontend', __name__, template_folder='templates', static_folder='static')
+frontend = Blueprint(
+    'frontend', __name__, template_folder='templates', static_folder='static')
 
+
+personajes = [
+    {
+        'nombre': 'gerald',
+        'arma': 'pistola'
+    },
+    {
+        'nombre': 'esen',
+        'arma': 'hacha'
+    }
+]
 
 
 @frontend.route('/')
 def index():
-	return "hello from frontend"
+    personajes.append({'nombre': 'carlos', 'arma': 'cuchillo'})
+
+    return render_template(
+        'index.html',
+        entidad='Tsathotugga',
+        nombre='Gerald',
+        amigos=['pedro', 'esen', 'karel'],
+        personajes=personajes
+    )
