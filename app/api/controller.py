f1103034056b974911bd38919e359e5ce0055f2d
ondoheer <ondoheer@gmail.com>
2014-11-08 16:46:45 -0500

    models

---
+++ app/api/controller.py
@@ -0,0 +1,9 @@
+from models import (
+    users_collection,
+    proffessions_collection,
+    items_collection,
+    skills_collection,
+    languages_collection,
+    spells_collection,
+    books_collection
+)
---
+++ app/api/models.py
@@ -0,0 +1,37 @@
+import pymongo
+
+# Connection to Mongo DB
+try:
+    conn = pymongo.MongoClient()
+    print "Connected successfully!!!"
+except pymongo.errors.ConnectionFailure as e:
+    print "Could not connect to MongoDB: {}}".format(e)
+
+
+# create and connect to the database
+db = conn.cthulhu
+
+# create and select collections
+
+# usuarios
+users_collection = db.users
+
+# profesiones
+proffessions_collection = db.proffessions
+
+# items
+items_collection = db.items
+
+# skills
+skills_collection = db.skills
+
+# languages
+languages_collection = db.languages
+
+# spells
+spells_collection = db.spells
+
+# books
+books_collection = db.books
+
+print db.books.count()
