07721a338badda0de3760cf6afeecba8e996cfb2
ondoheer <ondoheer@gmail.com>
2014-11-08 18:47:06 -0500

    rutas hechas

--- app/api/views.py
+++ app/api/views.py
@@ -6,5 +6,14 @@ api = Blueprint('api', __name__, url_prefix='/api')
 
 
 @api.route('/v1.0/creator', methods=['GET'])
-def getcharacters():
+def getCharacters():
     return jsonify({"user": 2, "message": "este es tu id de pj"})
+
+
+@api.route('/v1.0/creator/<int:character_id>', methods=['GET'])
+def getCharacter(character_id):
+    if character_id is 1:
+        return jsonify({'name': 'edward wisnton'})
+
+    else:
+        return "No existe ese pj"
