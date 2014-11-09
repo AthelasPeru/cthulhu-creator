34b2dbfa34619b67135a8b923bf9d4dee4a6dc23
ondoheer <ondoheer@gmail.com>
2014-11-08 20:51:08 -0500

    login hecho

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,19 +1,25 @@
 from flask import Blueprint, render_template
+from flask.ext.stormpath import StormpathError, login_required, User, login_user, logout_user, user
+
 
 frontend = Blueprint(
     'frontend', __name__, template_folder='templates', static_folder='static')
 
 
 @frontend.route('/')
+@frontend.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
 
 @frontend.route('/create')
+@login_required
 def create():
     return render_template('create.html')
 
 
 @frontend.route('/preview')
+@login_required
 def preview():
     return render_template('preview.html')
