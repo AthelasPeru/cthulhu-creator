65a1e069fa2873e4a1e52eb794a12e553095eb1b
Gerald <gerald@athelas.pe>
2015-03-03 09:49:18 -0500

    habiendo instalado gulp

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,46 @@
+$(document).ready(function(){
+
+
+// pagina 01
+
+	$(".page01").on("click", $(".creator_buttom"), function(){
+
+		$(".page03").removeClass("hide");
+		$(".page01").addClass("hide");
+		
+	});
+
+// pagina 03
+
+	$(".page03").on("click", $(".topage04"), function(){
+
+		$(".page05").removeClass("hide");
+		$(".page03").addClass("hide");
+
+
+	});
+
+
+
+// pagina 05
+
+	$(".page05").on("click", $(".topage06"), function(){
+
+		$(".page05").removeClass("hide");
+		$(".page03").addClass("hide");
+
+
+	});
+
+
+
+});
+
 ///////////////pagina de inicio//////////////////
 
-1	al dar click al boton "create your investigator" se debe abrir la siguiente pagina: "core rolls"
-2 al dar click al nombre de un personaje creado, se debe abrir la "pagina principal" de dicho personaje
-3	moviendo a derecha (o izquierda) aparecen mas nombres de investigadores (si la cuenta tiene mas de 3).
+// 1	al dar click al boton "create your investigator" se debe abrir la siguiente pagina: "core rolls"
+// 2 al dar click al nombre de un personaje creado, se debe abrir la "pagina principal" de dicho personaje
+// 3	moviendo a derecha (o izquierda) aparecen mas nombres de investigadores (si la cuenta tiene mas de 3).
 
 
 ///////////////////roll Calls////////////////
@@ -10,22 +48,22 @@
 
 
 ////////////ocupation///////////////////
-1	al dar hover sobre el boton, aparece la lista de profesiones, al dar click sobre una, esta se selecciona(cambia de color x ejenmplo)
-2	dar click sobre back te regresa a "roll calls"
-3	dar click al boton "next" te lleva a la pagina "ocupational skills"
+// 1	al dar hover sobre el boton, aparece la lista de profesiones, al dar click sobre una, esta se selecciona(cambia de color x ejenmplo)
+// 2	dar click sobre back te regresa a "roll calls"
+// 3	dar click al boton "next" te lleva a la pagina "ocupational skills"
 
 
-ocupational skills
+// ocupational skills //////////////////////////////////////
 
 
 
 
 //////////////hobby skills/////////////////
 
-1	se debe ver la lista completa de skills , en ella su porcentaje base y 2 botones para aumentar o disminuir puntaje
-2	el total de "hobby points" es INT * 10
-		cuando se da click a los botones de adicionar o restar, se va modificando este numero base
-3	cuando se selecciona un skill se puede editar en al cuadro de la derecha		
-4	dar click sobre back te regresa a "ocupational skills"
-5	dar click al boton "next" te lleva a la pagina "investigator data"
+// 1	se debe ver la lista completa de skills , en ella su porcentaje base y 2 botones para aumentar o disminuir puntaje
+// 2	el total de "hobby points" es INT * 10
+		// cuando se da click a los botones de adicionar o restar, se va modificando este numero base
+// 3	cuando se selecciona un skill se puede editar en al cuadro de la derecha		
+// 4	dar click sobre back te regresa a "ocupational skills"
+// 5	dar click al boton "next" te lleva a la pagina "investigator data"
 
